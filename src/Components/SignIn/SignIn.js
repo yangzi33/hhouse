1b1a19ed1d40310d75fc970bbc3a5277573111ae
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2023-02-10 00:11:08 -0500

    Updated dependencies; basic structure done

---
+++ src/Components/SignIn/SignIn.js
@@ -0,0 +1,13 @@
+// import { withRouter } from "react-router";
+import React from "react";
+import "../../Style/SignIn.css"
+
+class SignIn extends React.Component {
+  render(){
+    return <h1>Hello World</h1>
+  }
+}
+
+export default SignIn;
+
+ 
