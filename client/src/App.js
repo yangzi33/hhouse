aee224443b15ca8d8d0ac402cf0685ffdb082dd7
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2023-07-01 23:08:55 -0400

    original shitty navbars

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,23 @@ import "./App.css";
 import productData from "./dummy-data.json";
 import ProductCard from "./Components/product/ProductCard";
 import ProductTable from "./Components/product/ProductTable";
+import TopBar from "./Components/appbar/TopBar";
 import SideBar from "./Components/appbar/SideBar";
+import PersistentDrawerLeft from "./Components/appbar/PersistentDrawer";
 
 export default class App extends Component {
   render() {
     // console.log(productData.products);
+    const appContainer = { display: "flex" };
     return (
-      <div>
-        <SideBar />
-        <ProductTable className="product-table" products={productData} />
-      </div>
+      <>
+        {/* <TopBar /> */}
+        <PersistentDrawerLeft />
+        <div style={appContainer}>
+          {/* <SideBar /> */}
+          <ProductTable className="product-table" products={productData} />
+        </div>
+      </>
     );
   }
 }
